9c12e4da71bbccbcae61e22bc3efddf15e0c7651
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-03-11 20:30:47 +0100

    Implement format functions

--- src/CabalCargs/Format.hs
+++ src/CabalCargs/Format.hs
@@ -5,7 +5,36 @@ module CabalCargs.Format
 
 import CabalCargs.CompilerArgs (CompilerArgs(..))
 import CabalCargs.Formatting (Formatting(..))
+import Data.Maybe (maybeToList)
 
 
 format :: Formatting -> CompilerArgs -> [String]
-format fmt cargs = [""]
+format Ghc cargs = concat [ map ("-i" ++) (hsSourceDirs cargs)
+                          , ghcOptions cargs
+                          , map ("-X" ++) (defaultExtensions cargs)
+                          , map ("-optP" ++) (cppOptions cargs)
+--                          , map ("   " ++) (cSources cargs)
+                          , map ("-optc" ++) (ccOptions cargs)
+                          , map ("-L" ++) (extraLibDirs cargs)
+                          , map ("-l" ++) (extraLibraries cargs)
+--                          , map ("   " ++) (ldOptions cargs)
+                          , map ("-I" ++) (includeDirs cargs)
+--                          , map ("   " ++) (includes cargs)
+                          , maybe [""] (\db -> ["-package-db " ++ db]) (packageDB cargs)
+                          ]
+
+format Hdevtools cargs = map ("-g" ++) (format Ghc cargs)
+
+format Pure cargs = concat [ hsSourceDirs cargs
+                           , ghcOptions cargs
+                           , defaultExtensions cargs
+                           , cppOptions cargs
+                           , cSources cargs
+                           , ccOptions cargs
+                           , extraLibDirs cargs
+                           , extraLibraries cargs
+                           , ldOptions cargs
+                           , includeDirs cargs
+                           , includes cargs
+                           , maybeToList $ packageDB cargs
+                           ]
