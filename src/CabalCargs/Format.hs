10d7dc69c57b21e77195a7c9a3065731243ce0a4
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-03-11 20:42:19 +0100

    Add the socket argument for hdevtools

--- src/CabalCargs/Format.hs
+++ src/CabalCargs/Format.hs
@@ -1,3 +1,4 @@
+{-# Language PatternGuards #-}
 
 module CabalCargs.Format
    ( format
@@ -23,7 +24,13 @@ format Ghc cargs = concat [ map ("-i" ++) (hsSourceDirs cargs)
                           , maybe [""] (\db -> ["-package-db " ++ db]) (packageDB cargs)
                           ]
 
-format Hdevtools cargs = map ("-g" ++) (format Ghc cargs)
+format Hdevtools cargs = (map ("-g" ++) (format Ghc cargs)) ++ socket
+   where
+      socket | dirs@(_:_) <- hsSourceDirs cargs
+             = ["--socket=" ++ head dirs ++ "/.hdevtools.sock"]
+
+             | otherwise
+             = []
 
 format Pure cargs = concat [ hsSourceDirs cargs
                            , ghcOptions cargs
